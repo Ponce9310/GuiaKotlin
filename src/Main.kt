03b915056aceb377de2cde812d90b0f8b18df2c4
Ponce9310 <de.poncec@duocuc.cl>
2026-08-16 19:02:06 -0400

    Agrega resolución de Parte 1: Variables y Operadores

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+fun main(){
+    val numeroEntero : Int =10
+    val numeroDecimal : Double =2.5
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    val suma = numeroEntero + numeroDecimal
+    val resta = numeroEntero - numeroDecimal
+    val multiplicacion = numeroEntero * numeroDecimal
+    val division = numeroEntero / numeroDecimal
+
+    println("La suma es: $suma")
+    println("La resta es: $resta")
+    println("La multiplicacion es: $multiplicacion")
+    println("La division es: $division")
 }
