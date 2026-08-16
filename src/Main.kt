6594b1147ce3e055e0b6a673818cb41b6f5b3d43
Ponce9310 <de.poncec@duocuc.cl>
2026-08-16 19:29:31 -0400

    Agrega resolución de Parte 2: Seguridad ante Nulos (Null Safety)

--- src/Main.kt
+++ src/Main.kt
@@ -11,4 +11,16 @@ fun main(){
     println("La resta es: $resta")
     println("La multiplicacion es: $multiplicacion")
     println("La division es: $division")
+
+
+    println("\n--- PARTE 2: Seguridad ante Nulos ---")
+
+    var miTexto: String? = "Hola a todos"
+    println("La longitud del texto es: ${miTexto?.length}")
+
+    miTexto = null
+    println("La longitud ahora es: ${miTexto?.length}")
+
+
+
 }
